5f9449513cd766df5334b41e047dd7b9332987ff
PRASHANTBHATTI <98298142+PRASHANTBHATTI@users.noreply.github.com>
2026-06-19 10:19:24 -0700

    Change GROQ_API_KEY in ruhi_advanced.py
    Updated GROQ_API_KEY to a new value.

--- ruhi_advanced.py
+++ ruhi_advanced.py
@@ -5,7 +5,7 @@ from groq import Groq
 from dotenv import load_dotenv
 
 load_dotenv()
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("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")
 client = Groq(api_key=GROQ_API_KEY)
 
 # ============================================================================
